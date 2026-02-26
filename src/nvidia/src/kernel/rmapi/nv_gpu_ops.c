26ac87079e3e879c5224e352b795fc78421e2d20
miaotianxiang <miaotianxiang@baidu.com>
2026-02-26 10:19:12 +0800

    20260226 ampere_uvm_debug

--- src/nvidia/src/kernel/rmapi/nv_gpu_ops.c
+++ src/nvidia/src/kernel/rmapi/nv_gpu_ops.c
@@ -2150,11 +2150,11 @@ NV_STATUS nvGpuOpsDupAddressSpace(struct gpuDevice *device,
             goto cleanup_dup_vaspace;
     }
 
-    if (!vaspaceIsExternallyOwned(pVAS))
-    {
-        status = NV_ERR_INVALID_FLAGS;
-        goto cleanup_dup_vaspace;
-    }
+    //if (!vaspaceIsExternallyOwned(pVAS))
+    //{
+    //    status = NV_ERR_INVALID_FLAGS;
+    //    goto cleanup_dup_vaspace;
+    //}
 
     status = getAddressSpaceInfo(gpuVaSpace, pGpu, vaSpaceInfo);
     if (status != NV_OK)
